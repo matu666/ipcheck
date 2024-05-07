0ec8ea831b4f1981468db802f53bc5eddef7a164
matu666 <matu6@qq.com>
2024-05-07 15:26:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+// 路由组件的懒加载
+const MTRTest = () => import('../components/mtrtest.vue');
+const PingTest = () => import('../components/globallatency.vue');
+const RuleTest = () => import('../components/ruletest.vue');
+const DNSResolver = () => import('../components/dnsresolver.vue');
+const CensorshipCheck = () => import('../components/censorshipcheck.vue');
+const EmptyComponent = () => import('../components/empty.vue');
+
+const routes = [
+  { path: '/', component: EmptyComponent },
+  { path: '/mtrtest', component: MTRTest },
+  { path: '/pingtest', component: PingTest },
+  { path: '/ruletest', component: RuleTest },
+  { path: '/dnsresolver', component: DNSResolver },
+  { path: '/censorshipcheck', component: CensorshipCheck },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
